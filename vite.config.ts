f7e0ba1a2e7dd21e882f0f6fc57e0dcbf400281b
kassuro <l.fuhl@demv.de>
2021-05-17 17:30:46 +0200

    setup ts path alias usage for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import eslintPlugin from 'vite-plugin-eslint';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
       cache: false,
       include: '**/*.+(vue|js|jsx|ts|tsx)',
     }),
+    tsconfigPaths(),
   ],
 });
