56448060c59173c0b84eb0ebaf049c0331cab31e
kassuro <l.fuhl@demv.de>
2021-05-17 17:41:21 +0200

    use linting to fix up existing code

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <template>
-  <div>
-    Hello Vue
-  </div>
+  <div>Hello Vue</div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
-
+import { defineComponent } from 'vue';
 
 export default defineComponent({
   name: 'App',
-
-})
+});
 </script>
