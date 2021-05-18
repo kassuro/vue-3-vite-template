128ebb91720682b42e88414353d0f63400703df8
kassuro <l.fuhl@demv.de>
2021-05-18 11:32:45 +0200

    add vite config for change port via env var

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: Number.parseInt(process.env.PORT) || 3000,
+  },
   plugins: [
     vue(),
     eslintPlugin({
